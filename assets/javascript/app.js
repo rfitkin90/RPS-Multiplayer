cea7e3017cabc936b1cfd86af896572d34a8ac31
Richard Fitkin <rfitkin90@gmail.com>
2019-01-03 21:29:27 -0800

    score is going back down after incrementing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,10 +15,13 @@ $(document).ready(function () {
     // declare user variables
     var user1;
     var user2;
-    var playingAs = "notPlaying";
+    var playingAs = 'notPlaying';
     var phase;
-    var playerOneScore = 0;
-    var playerTwoScore = 0;
+
+    // reset database
+    // database.ref().set({ phase: 'waitingForPlayers' });
+    // database.ref('user1').set({ connected: false, name: null, score: 0 });
+    // database.ref('user2').set({ connected: false, name: null, score: 0 });
 
     // get current user data from database
     database.ref().on('value', function (snap) {
@@ -56,20 +59,28 @@ $(document).ready(function () {
         $('#player-2-name').text(user2.name);
 
         // get current scores from database
-        $('#player-1-score').text(playerOneScore);
-        $('#player-2-score').text(playerTwoScore);
+        $('#player-1-score').text(user1.score);
+        $('#player-2-score').text(user2.score);
 
         // compare moves
         if (phase === "moveSelection") {
-            if ((user1.move === rock && user2.move === scissors) ||
-                (user1.move === scissors && user2.move === paper) ||
-                (user1.move === paper && user2.move === rock)) {
+            if ((user1.move === 'rock' && user2.move === 'scissors') ||
+                (user1.move === 'scissors' && user2.move === 'paper') ||
+                (user1.move === 'paper' && user2.move === 'rock')) {
+                roundTransition('Player 1', user1.score, 'user1');
+            } else if ((user2.move === 'rock' && user1.move === 'scissors') ||
+                (user2.move === 'scissors' && user1.move === 'paper') ||
+                (user2.move === 'paper' && user1.move === 'rock')) {
+                roundTransition('Player 2', user2.score, 'user2');
+            } else if ((user1.move === 'rock' && user2.move === 'rock') ||
+                (user1.move === 'scissors' && user2.move === 'scissors') ||
+                (user1.move === 'paper' && user2.move === 'paper')) {
                 phase = "roundTransition";
+                database.ref("phase").set("roundTransition");
+                console.log(phase);
                 $('#player-1-move').text(user1.move);
                 $('#player-2-move').text(user2.move);
-                $('#winner-result').text('Player 1');
-                playerOneScore++;
-                $('#player-1-score').text(playerOneScore);
+                $('#winner-result').text('Draw');
                 $('#round-result').css('visibility', 'visible');
                 $('#next-round-counter').text('Next round in 3...');
                 setTimeout(() => {
@@ -79,6 +90,7 @@ $(document).ready(function () {
                         setTimeout(() => {
                             $('#round-result').css('visibility', 'hidden');
                             phase = "moveSelection";
+                            database.ref("phase").set("moveSelection");
                             $('#player-1-move').text('');
                             $('#player-2-move').text('');
                         }, 1000);
@@ -87,6 +99,34 @@ $(document).ready(function () {
             }
         }
 
+        function roundTransition(playerNumber, userScore, userRef) {
+            phase = "roundTransition";
+            database.ref("phase").set("roundTransition");
+            $('#player-1-move').text(user1.move);
+            $('#player-2-move').text(user2.move);
+            $('#winner-result').text(playerNumber);
+            console.log(userScore);
+            console.log(phase);
+            console.log(playingAs);
+            if (playingAs === userRef) { userScore++ };
+            database.ref(userRef).update({ score: userScore });
+            $('#player-1-score').text(user1.score);
+            $('#player-2-score').text(user2.score);
+            $('#round-result').css('visibility', 'visible');
+            $('#next-round-counter').text('Next round in 3...');
+            setTimeout(() => {
+                $('#next-round-counter').text('Next round in 2...');
+                setTimeout(() => {
+                    $('#next-round-counter').text('Next round in 1...');
+                    setTimeout(() => {
+                        $('#round-result').css('visibility', 'hidden');
+                        phase = "moveSelection";
+                        $('#player-1-move').text('');
+                        $('#player-2-move').text('');
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }
     });
 
     // player joins game
@@ -95,17 +135,20 @@ $(document).ready(function () {
         // join the first available seat
         if (user1.connected === false && playingAs === 'notPlaying') {
             joinGame(user1.connected, user1.name, 'user1');
-            phase = "moveSelection";
-            database.ref("phase").set("moveSelection");
         } else if (user2.connected === false && playingAs === 'notPlaying') {
             joinGame(user2.connected, user2.name, 'user2');
-            phase = "moveSelection";
-            database.ref("phase").set("moveSelection");
         } else if (playingAs === 'user1' || playingAs === 'user2') {
             alert('Already playing.');
         } else {
             alert('Game is full.');
         }
+
+        if (user1.connected === true && user2.connected === true) {
+            phase = "moveSelection";
+            database.ref("phase").set("moveSelection");
+        }
+
+        console.log(phase);
 
         function joinGame(userConnected, userName, userSlot) {
             // update local variables
@@ -115,7 +158,7 @@ $(document).ready(function () {
             playingAs = userSlot;
             console.log(playingAs);
             // update database
-            database.ref(userSlot).set({
+            database.ref(userSlot).update({
                 connected: true,
                 name: userName,
             });
@@ -127,13 +170,17 @@ $(document).ready(function () {
     // move selection
     $(document).on('click', '.move-selection', function (e) {
         e.preventDefault();
+        console.log('sup');
+        console.log(playingAs);
+        console.log(user1.move);
+        console.log(user2.move);
         if (phase === "moveSelection") {
-            if (playingAs === 'user1' && user1.move === null) {
+            if (playingAs === 'user1' && user1.move === undefined) {
                 user1.move = $(this).attr('data-move');
                 console.log(user1.move);
                 database.ref('user1').update({ move: user1.move });
                 $('#player-1-move').text('Chosen');
-            } else if (playingAs === 'user2' && user2.move === null) {
+            } else if (playingAs === 'user2' && user2.move === undefined) {
                 user2.move = $(this).attr('data-move');
                 console.log(user2.move);
                 database.ref('user2').update({ move: user2.move });
